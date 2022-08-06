e4b1767cb05641cdd3b429822f3a444dc917be15
Arabelle <yulitoniy@gmail.com>
2022-08-06 18:25:38 +0300

    final of code

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 // Первоначальный массив можно задать или ввести с клавиатуры.
 
 string[] array = new string[]{
-        "1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02",
-        "1S02", "1H01", "1H01", "1S02", "1S01", "1S01", "1H01",
+        "1S01", ">kz", "1S01", "1S01", "1S01", "1S02", "1S02",
+        "1S02", "1H01", "1H01", "1S02", "1S01", "1", "1H01",
         "1H01", "1H01", "1S02", "Юля", "1M02", "1M02", "1M02"};
 
 Console.WriteLine("[" + string.Join(", ", array) + "]");
@@ -12,15 +12,17 @@ Console.WriteLine("[" + string.Join(", ", array) + "]");
 void SelectThreeSymbol(string[] array) {
     int lenOfarray = array.Length;
     int count = 3; // magic number of symbol form the task
-    string[] arrayTemp = new string[lenOfarray];
+    string[] arraySelect = new string[lenOfarray];
     int countOfString = 0;
+
     for(int i = 0; i < lenOfarray; i++) {
         if(array[i].Length <= count) {
-            arrayTemp[countOfString] = array[i];
+            arraySelect[countOfString] = array[i];
             countOfString++;
         }
     }
-    Console.WriteLine("[" + string.Join(", ", arrayTemp) + "]");
+    Array.Resize(ref arraySelect, countOfString);
+    Console.WriteLine("[" + string.Join(", ", arraySelect) + "]");
 }
 
 SelectThreeSymbol(array);
